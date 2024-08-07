cba969bae21c8e067869ecee1ea7555918fec0d6
haitam-elgharras <elgharras.haitam@gmail.com>
2024-08-07 15:56:20 +0100

    play with resilience4j retry config

--- currency-services/currency-exchange-service/src/main/java/com/in28minutes/microservices/currencyexchangeservice/CircuitBreakerController.java
+++ currency-services/currency-exchange-service/src/main/java/com/in28minutes/microservices/currencyexchangeservice/CircuitBreakerController.java
@@ -1,14 +1,27 @@
 package com.in28minutes.microservices.currencyexchangeservice;
 
 
+import io.github.resilience4j.retry.annotation.Retry;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.logging.Logger;
 
 @RestController
 public class CircuitBreakerController {
+    private final Logger logger = Logger.getLogger(CircuitBreakerController.class.getName());
 
     @GetMapping("/sample-api")
+    @Retry(name="sample-api", fallbackMethod = "defaultFailResponse")
     public String sampleApi() {
-        return "Sample API";
+        logger.info("Sample API call received");
+
+        RestTemplate restTemplate = new RestTemplate();
+        return restTemplate.getForEntity("http://localhost:8080/some-dummy-url", String.class).getBody();
+    }
+
+    public String defaultFailResponse(Exception e) {
+        return "Service failed to reply";
     }
 }
