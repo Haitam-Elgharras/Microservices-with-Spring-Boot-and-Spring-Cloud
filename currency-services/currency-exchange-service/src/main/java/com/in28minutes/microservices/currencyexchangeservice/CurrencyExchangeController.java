c095baa4f61c7c8e2f1f5bfde26f61ead0cb5297
haitam-elgharras <elgharras.haitam@gmail.com>
2024-08-08 16:59:52 +0100

    set up Distributed Tracing

--- currency-services/currency-exchange-service/src/main/java/com/in28minutes/microservices/currencyexchangeservice/CurrencyExchangeController.java
+++ currency-services/currency-exchange-service/src/main/java/com/in28minutes/microservices/currencyexchangeservice/CurrencyExchangeController.java
@@ -1,21 +1,24 @@
 package com.in28minutes.microservices.currencyexchangeservice;
 
-import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.math.BigDecimal;
-
 @RestController
-@AllArgsConstructor
+@RequiredArgsConstructor
 public class CurrencyExchangeController {
-    private Environment environment;
-    private CurrencyExchangeRepository repository;
+    private final Environment environment;
+    private final CurrencyExchangeRepository repository;
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
 
     @GetMapping("/currency-exchange/from/{from}/to/{to}")
     public CurrencyExchange retrieveExchangeValue(@PathVariable String from, @PathVariable String to) {
+        logger.info("retrieveExchangeValue called with {} to {}", from, to);
+
         String port = environment.getProperty("local.server.port");
         CurrencyExchange currencyExchange = repository.findByFromAndTo(from, to);
 
